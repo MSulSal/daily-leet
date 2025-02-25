e97922aff3906f1353da17e07c33af68c43aecb2
MSulSal <msulsaleem@gmail.com>
2025-02-24 20:05:08 -0600

    Contains Duplicate: c#

---
+++ 217. Contains Duplicate/Solution.cs
@@ -0,0 +1,13 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        ISet<int> numset = new HashSet<int>();
+
+        foreach (int num in nums) {
+            if (numset.Contains(num)) {
+                return true;
+            }
+            numset.Add(num);
+        }
+        return false;
+    }
+}
